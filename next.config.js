a94185844defc16dece3af31527bce21f27011e4
sukronsabari11 <sukronsabari11@gmail.com>
2023-03-11 18:20:43 +0800

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+});
+
+module.exports = withPWA({
   reactStrictMode: false,
   images: {
     loader: 'default',
@@ -19,6 +28,4 @@ const nextConfig = {
       },
     ],
   },
-};
-
-module.exports = nextConfig;
+});
