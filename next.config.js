7bc8243ffca0116b4f3dcfbd8f12dd968e1bfe2a
sukronsabari11 <sukronsabari11@gmail.com>
2023-03-13 14:28:55 +0800

    Edit manifest.json

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  scope: '/app',
   // register: true,
-  // scope: '/app',
   // sw: 'service-worker.js',
   //...
 });
